0ffa0e0a02fd2ed77c367938ae86ceadb8e5b276
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2023-10-28 21:18:43 -0400

    Update MultiHeadAttentionTest.py

--- MultiHeadAttentionTest.py
+++ MultiHeadAttentionTest.py
@@ -1,102 +1,103 @@
-from torch import nn, tensor
 import torch
-from layers import get_question_and_answer, Train
 torch.random.manual_seed(42)
 torch.cuda.manual_seed_all(42)
 torch.backends.cudnn.deterministic = True
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class Seq2Seq(torch.nn.Module):
-    def __init__(self, corpus, embedding_dim, hidden_size, num_head, num_layers):
-        super().__init__()
-        self.encoder = Encoder(corpus, embedding_dim, hidden_size, num_layers)
-        self.decoder = Decoder(corpus, embedding_dim, hidden_size, num_head, num_layers)
-        self.loss = nn.CrossEntropyLoss()
+class MultiHeadAttention:
+    def __init__(self, hiddens, num_heads=8):
+        self.Wq = torch.nn.Linear(hiddens, hiddens)
+        self.Wk = torch.nn.Linear(hiddens, hiddens)
+        self.Wv = torch.nn.Linear(hiddens, hiddens)
+        self.Wo = torch.nn.Linear(hiddens, hiddens)
+        self.num_heads = num_heads
 
-    def forward(self, x, target):
-        decoder_input, decoder_target = target[:, :-1], target[:, 1:]
-        x, h_n = self.encoder(x)
-        new_sequence = self.decoder(decoder_input, x, h_n)
-        loss = self.loss(torch.permute(new_sequence, (0, 2, 1)), decoder_target)
-        return loss
+    def forward(self, query, key, value):
+        q = self.Wq(query)
+        k = self.Wk(key)
+        v = self.Wv(value)
 
-    def generate(self, question, word_id, size):
-        x, h_n = self.encoder.forward(question)
-        answer = self.decoder.generate(x, h_n, tensor(word_id['_'], device=device).reshape(1, 1), size)
-        return answer
+        # split the tensor into num_heads
+        """
+        assume split into 3 heads
+        a = array([[[ 0,  1,  2],
+                    [ 3,  4,  5],
+                    [ 6,  7,  8],
+                    [ 9, 10, 11],
+                    [12, 13, 14]],
+
+                   [[15, 16, 17],
+                    [18, 19, 20],
+                    [21, 22, 23],
+                    [24, 25, 26],
+                    [27, 28, 29]]])
+        a.reshape(a.shape[0], a.shape[1], 3, int(b.shape[2] / 3))
+        a[:, :, 0, :] = array([[[ 0],
+                                [ 3],
+                                [ 6],
+                                [ 9],
+                                [12]],
+                        
+                               [[15],
+                                [18],
+                                [21],
+                                [24],
+                                [27]]])
+        """
+        q = q.reshape(q.shape[0], q.shape[1], self.num_heads, int(q.shape[2] / self.num_heads)).permute(0, 2, 1, 3)
+        k = k.reshape(k.shape[0], k.shape[1], self.num_heads, int(k.shape[2] / self.num_heads)).permute(0, 2, 1, 3)
+        v = v.reshape(v.shape[0], v.shape[1], self.num_heads, int(v.shape[2] / self.num_heads)).permute(0, 2, 1, 3)
+
+        # transpose it so that we can calculate the dot product for all vectors
+        """
+        q = (batch, num_heads, sentence_length, each_head_hidden)
+        transposed_shape = (batch, num_heads, each_head_hidden, sentence_length)
+        score table = (batch, num_heads, sentence_length, sentence_length)
+        Each element in the score table means the hidden weight relationship between the query and the key.
+        """
+        transposed = k.permute(0, 1, 3, 2)
+        weight = q @ transposed
+
+        scaled_score = weight / torch.sqrt(torch.tensor(int(v.shape[2] / self.num_heads)))
+
+        """
+        assume table is (5, 5), where first 10 means the word in the sentence, and the second 10 means the weight of
+        the word in the sentence based on the first word.
+                         Today  is   a   good   day
+        table = Today   [[0.1, 0.2, 0.6, 0.05, 0.05]
+                is       [0.05, 0.6, 0.2, 0.1, 0.05]
+                a        [0.1, 0.6, 0.2, 0.05, 0.05]
+                good     [0.1, 0.2, 0.1, 0.3,   0.3]
+                day      [0.1, 0.2, 0.6, 0.05, 0.05]]
+        We can see that the score table tells us that the word "Today" has a strong relationship with the word "a".
+        """
+        weight = torch.softmax(scaled_score, dim=-1)
+
+        """
+        now we get the possibility (batch, num_heads, sentence_length, possibility)
+        where last two axes means there are sentence_length vectors with size possibility,
+         and each vector has possibility.
+        Since the hidden size is 512, which means we have 64 vectors for each head on the right operand.
+        Since the score table is (10, 10), which means we have 10 vectors on the left operand.
+        We use matmul to calculate the dot product for all vectors and finally we have 640 vectors for each head.
+        Each element in the vector means the word the model is focusing on based on the weight.
+        
+        The Output is like extract the information from the sentence, and each word get a new hidden vector compare with
+        the original hidden vector because we added small partial of hidden weights from other words.
+        """
+        output = torch.matmul(weight, v)
+
+        # reshape back to the original shape
+        merged = output.permute(0, 2, 1, 3).reshape(output.shape[0], output.shape[2], -1)
+        return self.Wo(merged)
 
 
-class Encoder(torch.nn.Module):
-    def __init__(self, corpus, embedding_dim, hidden_size, num_layers):
-        super().__init__()
-        self.word_embedding = nn.Embedding(corpus, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers, batch_first=True)
+layer = MultiHeadAttention(512)
+inp = torch.randn(64, 10, 512)
+key = torch.randn(64, 10, 512)
+value = torch.randn(64, 10, 512)
+res = layer.forward(inp, key, value)
+res.mean().backward()
+assert res.shape == torch.Size([64, 10, 512])
 
-    def forward(self, x):
-        x = self.word_embedding.forward(x)
-        x, (h_n, c_n) = self.lstm.forward(x)
-        return x, h_n
-
-
-class Decoder(torch.nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_size, num_head, num_layers):
-        super().__init__()
-        self.word_embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers, batch_first=True)
-        self.lstm2 = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True)
-        self.linear3 = nn.Linear(hidden_size, vocab_size)
-        self.multi_attention = nn.MultiheadAttention(hidden_size, num_head, batch_first=True)
-        self.h_n, self.c_n = None, None
-        self.hidden_size = hidden_size
-        self.embedding_dim = embedding_dim
-
-    def forward(self, answer, hidden_state, h_n, stateful=False):  # hidden_state (batch, h_t, hidden_size)
-        h_n = self.h_n if self.h_n is not None else h_n
-        c_n = self.c_n if self.c_n is not None else torch.zeros(h_n.shape, device=h_n.device)
-        x = self.word_embedding.forward(answer)
-        new_sequence, (h_n, c_n) = self.lstm.forward(x, (h_n, c_n))
-        new_sequence, weights = self.multi_attention.forward(new_sequence, hidden_state, hidden_state)
-        if stateful:
-            self.h_n = h_n
-            self.c_n = c_n
-        else:
-            self.h_n, self.c_n = None, None
-        new_sequence = self.linear3.forward(new_sequence)
-        return new_sequence
-
-    def generate(self, enc_hs, h_n, start_id, sample_size):
-        sampled = []
-        sample_id = start_id
-        total_hidden = enc_hs
-        last_hidden = h_n
-        for _ in range(sample_size):
-            x = sample_id.reshape((1, 1))
-            score = self.forward(x, total_hidden, last_hidden, stateful=True)
-            sample_id = torch.argmax(score.flatten())
-            sampled.append(sample_id)
-        self.h_n, self.c_n = None, None
-        return sampled
-
-
-batch = 100
-sentence_length = 30
-train_questions, test_questions, train_answer, test_answer, word_id, id_word = get_question_and_answer(
-    r"C:\Users\123\PycharmProjects\torch-models\data-set\symbolic computation\division_shuffle.txt",
-    torch=True, train_ratio=0.96)
-vocab_size = len(word_id)
-wordvec_size = 16
-hidden_size = 128
-max_epoch = 100
-num_head = 8
-num_layers = 1
-output_dim = 64
-seq2seq = Seq2Seq(vocab_size, wordvec_size, hidden_size, num_head, num_layers)
-seq2seq.to(device)
-optimizer = torch.optim.Adam(seq2seq.parameters())
-trainer = Train(seq2seq, optimizer)
-seq2seq.train()
-trainer.PYTORCH_train(train_questions, train_answer, test_questions, test_answer, batch,
-                      max_epoch, word_id, log_dir="division", log=True,
-                      log_file_name="Encoder1LSTM, Decoder1LSTM + 1Attention + affine")
-torch.save(seq2seq.state_dict(), 'model_weights.pth')
